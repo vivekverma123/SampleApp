27bd6c9b50ce3591a34efb20b0fa6cebf0d0af2c
vivekverma123 <123vivekverma@gmail.com>
2021-07-12 00:31:57 +0530

    Code changed

--- src/main/java/com/example/sampleapp/sampleapp/rest/Controller.java
+++ src/main/java/com/example/sampleapp/sampleapp/rest/Controller.java
@@ -3,9 +3,12 @@ package com.example.sampleapp.sampleapp.rest;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class Controller {
@@ -17,5 +20,11 @@ public class Controller {
         mp1.put("Name","Vivek");
         mp1.put("Age","21");
         return mp1;
+    }
+
+    @RequestMapping(value="", method=RequestMethod.GET)
+    public String getStatus()
+    {
+        return "Ok";
     }
 }
